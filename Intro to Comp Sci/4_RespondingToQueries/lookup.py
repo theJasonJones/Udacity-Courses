fb61a926d1eab9100437ee544ec6db59f912a308
TheJasonJones <jkjmr6@gmail.com>
2014-01-31 18:33:33 -0600

    Added lookup module

---
+++ Intro to Comp Sci/4_RespondingToQueries/lookup.py
@@ -0,0 +1,21 @@
+''' Define a procedure, lookup, that takes two inputs:
+  - an index: list
+  - keyword: string
+
+ The procedure should return a list of the urls associated with the keyword. If the keyword
+ is not in the index, the procedure should return an empty list. '''
+
+index = [['udacity', ['http://udacity.com', 'http://npr.org']],
+         ['computing', ['http://acm.org']]]
+
+def lookup(index,keyword):
+    i = 0
+    while i < len(index): 
+        if (index[i][0] == keyword):
+            return index[i][1]
+        i += 1
+
+print lookup(index,'udacity')
+print lookup(index, 'computing')
+#>>> ['http://udacity.com','http://npr.org']
+#>>> ['http://acm.org']
