f5ed3569db9ce6ab59d6d44bd8318b8c7c26d00c
TheJasonJones <jkjmr6@gmail.com>
2014-02-02 14:17:33 -0600

    Added a simple script to measure function execution

--- Intro to Comp Sci/5_HowProgramsWork/measureTime.py
+++ Intro to Comp Sci/5_HowProgramsWork/measureTime.py
@@ -3,16 +3,18 @@ import time
 
 def time_execution(code):
   start = time.clock()
-  result = eval.clock()
+  result = eval(code)
   run_time = time.clock() - start
   return result, run_time
 
+#To be honest, this really doesn't do much of anything
 def spin_loop(n):
   i = 0
   while i < n:
     i += 1
 
-#test cases
-print time_execution('spin_loop(1000)')
-print time_execution('spin_loop(100000)')
-print time_execution('spin_loop(1000000)')
+#test cases where each should increase by a factor of ~10
+print time_execution('spin_loop(10 ** 5)') #ten thousand
+print time_execution('spin_loop(10 ** 6)') #ten million
+print time_execution('spin_loop(10 ** 7)')
+print time_execution('spin_loop(10 ** 8)')
