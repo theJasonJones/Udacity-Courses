faaca1ad60858a9c16b2ae8c97a6c9aae229d529
TheJasonJones <jkjmr6@gmail.com>
2014-02-04 14:23:31 -0600

    Added file from Lesson 9

---
+++ Intro to Comp Sci/9_Modules/classes/resturant.py
@@ -0,0 +1,17 @@
+class Restaurant(object):
+    """
+    Represents a place that serves food.
+    """
+    def __init__(self, name):
+        self.name = name
+
+    def display(self):
+        """Display the restaurant."""
+        print self.name
+    
+    # define a method is_yummy
+    # that returns a boolean value of yummyness
+    # for now it should always return False
+    def is_yummy(self):
+        """Determine whether the resturant is good or not"""
+        return False
---
+++ Intro to Comp Sci/9_Modules/classes/studentMain.py
@@ -0,0 +1,11 @@
+# Please edit the Restaurant class in the file restaurant.py
+
+from restaurant import Restaurant
+
+# These are test cases
+
+mcdowells = Restaurant("McDowell's Hamgurger Emporium")
+print mcdowells.is_yummy()
+
+chez_bananas = Restaurant("Chez Bananas Crib of Contentment")
+print chez_bananas.is_yummy()
