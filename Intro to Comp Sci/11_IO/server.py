f9fc2940f361cf490c8b5f51d03d397f09306809
TheJasonJones <jkjmr6@gmail.com>
2014-02-05 20:34:45 -0600

    Added script for starting server

---
+++ Intro to Comp Sci/11_IO/server.py
@@ -0,0 +1,12 @@
+import web 
+
+from search import lucky_search
+from crawler import crawl_web
+
+class LuckySearch(object):
+  def GET(self, query):
+    return lucky_search([], corpus, query)
+
+corpus = crawl_web('http://udacity.com/cs101x/urank/index.html')
+app = web.application(('/(.*)', 'LuckySearch'), globals())
+app.run()
